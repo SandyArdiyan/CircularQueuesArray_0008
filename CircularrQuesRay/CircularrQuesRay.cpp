ba40c7d55e4ec086016874052a624e21773f9f50
SandyArdiyan <sanardiyan881@gmail.com>
2024-05-30 18:56:36 +0700

    8

--- CircularrQuesRay/CircularrQuesRay.cpp
+++ CircularrQuesRay/CircularrQuesRay.cpp
@@ -13,23 +13,21 @@ public:
 
 	void insert() {
 		int num;
-		cout << "Enter a number: ";
+		cout << "Enter a number : ";
 		cin >> num;
 		cout << endl;
 
-		// cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-			cout << "\nQueue overflow\n";
+			cout << "\nQueue overFlow\n";
 			return;
 		}
 
-		// cek apakah antrian kosong
 		if (FRONT == -1) {
 			FRONT = 0;
 			REAR = 0;
 		}
 		else {
-			// jika  REAR berada di posisi terakhir array, kembali ke awal array
+
 			if (REAR == max - 1)
 				REAR = 0;
 			else
@@ -39,46 +37,61 @@ public:
 	}
 
 	void remove() {
-		// cek apakah antrian kosong
+		//cek apakah antrian kosong
 		if (FRONT == -1) {
 			cout << "Queue underflow\n";
 			return;
 		}
-		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+		cout << "\nlement deleted from the Queue is : " << queue_array[FRONT] << "\n";
 
-		//cek jika antrian hanya satu elemen
+		//cek jika antrian hanya memiliki satu elemen
 		if (FRONT == REAR) {
 			FRONT = -1;
 			REAR = -1;
 		}
-		else
-			// jika elemen yang di hapus berada di posisi terakhir array, kembali ke awal array
+		else {
+			//jika elemen yang di hapus berada di posos terakhir array, kembali ke awal array
 			if (FRONT == max - 1)
-				FRONT = 0;
+				FRONT == 0;
 			else
 				FRONT = FRONT + 1;
+		}
 	}
-}
-
 	void display() {
-		int FRONT_position = FRONT;
-		int REAR_position = REAR;
+		int FRONT_postition = FRONT;
+		int REAR_postition = FRONT;
 
-		// cek apakah antrian kosong
+		//cek apakah antrian kosong
 		if (FRONT == -1) {
 			cout << "Queue is empty\n";
 			return;
 		}
 
-		cout << "\nElements in the queue are...\n";
+		cout << "\nElements in the Queue are ...\n";
 
-		//jika FRONT_position <= REAR_posistion, iterasi daari FRONT hingga REAR
-		if (FRONT_posistion <= REAR_position) {
-			while (FRONT_position <= REAR_posistion) {
-				cout << (queue_array[FRONT_posistion] << " ";
-				FRONT_position++;
+		// jika FRONT_poostition <= REAR_postition, iterai dari FRONT hingga REAR
+		if (FRONT_postition <= REAR_postition) {
+			while (FRONT_postition <= REAR_postition) {
+				cout << queue_array[FRONT_postition] << " ";
+				FRONT_postition++;
 			}
 			cout << endl;
 		}
-		else
-			// jika FRONT_posistion > REAR_position, iterasi dari FRONT hingga akhir arrray
+		else {
+			//jika FRONT_postition >REAR_postition, iterasi dari FRONT hingga akhri array
+			while (FRONT_postition <= max - 1) {
+				cout << queue_array[FRONT_postition] << " ";
+				FRONT_postition++;
+			}
+
+			FRONT_postition = 0;
+
+			//literasi dari awal array hingga REAR
+			while (FRONT_postition <= REAR_postition) {
+				cout << queue_array[FRONT_postition] << " ";
+				FRONT_postition++;
+			}
+			cout << endl;
+		}
+	}
+};
