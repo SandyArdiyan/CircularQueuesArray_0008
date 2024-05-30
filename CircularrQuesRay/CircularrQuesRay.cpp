c9aebbd11e051ba742c6df12006bd37f8ba37465
SandyArdiyan <sanardiyan881@gmail.com>
2024-05-30 17:45:33 +0700

    2

--- CircularrQuesRay/CircularrQuesRay.cpp
+++ CircularrQuesRay/CircularrQuesRay.cpp
@@ -1,4 +1,4 @@
-#include
+#include <iostream>
 using namespace std;
 
 class Queues {
@@ -9,3 +9,38 @@ public:
 	Queues() {
 		FRONT = -1;
 		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		// cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// jika  REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	void remove() {
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+		}
+	}
+};
